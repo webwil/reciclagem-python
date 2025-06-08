84de39887aa229db672f5222c59384e1ad3993c8
Willian Garcia <webwil2012@gmail.com>
2025-06-08 00:40:39 -0300

    Adicionado funcionalidade para soma de dois numeros

--- exemplo_01.py
+++ exemplo_01.py
@@ -4,3 +4,11 @@ nome = str(input("Digite seu nome: "))
 numero_caracteres = len(nome)
 
 print(f"O nome {nome} possui {numero_caracteres} caracteres")
+
+# Criando recurso que realiza a soma de 2 valores fornecidos pelo usuario
+primeiro_numero = int(input("Digite o primeiro numero: "))
+segundo_numero = int(input("Digite o segundo numero: ")) 
+
+soma = primeiro_numero + segundo_numero
+
+print(f"A soma dos dois numeros é {soma}")
