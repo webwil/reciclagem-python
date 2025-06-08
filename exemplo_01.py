3c94f6eaade0676944f1b2c38bd85354b4477f4e
Willian Garcia <webwil2012@gmail.com>
2025-06-08 00:24:52 -0300

    Adicionado funcionalidade que exibe o numero de caracteres

--- exemplo_01.py
+++ exemplo_01.py
@@ -1,3 +1,6 @@
 nome = str(input("Digite seu nome: "))
 
-print(f"Seu nome é {nome}")
+# Exibindo o numero de caracteres do numero digitado
+numero_caracteres = len(nome)
+
+print(f"O nome {nome} possui {numero_caracteres} caracteres")
