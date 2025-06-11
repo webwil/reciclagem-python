8b5beb7388b9a16874e6debf3a87f07212455f1a
Willian Garcia <webwil2012@gmail.com>
2025-06-11 05:34:50 -0300

    Atualizando com arquivo de revisão

---
+++ revisao_01.py
@@ -0,0 +1,42 @@
+'''
+INTEIROS (int)
+
+# 1 - Escreva um programa que soma dois números inteiros inseridos pelo usuário.
+primeiro_numero = int(input("Digite o primeiro numero: "))
+segundo_numero = int(input("Digite o segundo numero: "))
+soma = primeiro_numero + segundo_numero
+print(f"A soma de {primeiro_numero} mais {segundo_numero} é {soma}")
+
+# 2 - Crie um programa que receba um número do usuário e calcule o resto da divisão desse número por 5.
+numero = int(input("Digite o numero: "))
+divisor = 5
+divisao = numero % divisor
+print(f"O resto da divisão de {numero} dividido po {divisor} é {divisao}")
+
+# 3 - Desenvolva um programa que multiplique dois números fornecidos pelo usuário e mostre o resultado.
+primeiro_numero = int(input("Digite o primeiro numero: "))
+segundo_numero = int(input("Digite o segundo numero: "))
+multiplicacao = primeiro_numero * segundo_numero
+print(f"o numero {primeiro_numero} multiplicado por {segundo_numero} é igual a: {multiplicacao}")
+
+# 4 - Faça um programa que peça dois números inteiros e imprima a divisão inteira do primeiro pelo segundo.
+primeiro_numero = int(input("Digite o primeiro numero: "))
+segundo_numero = int(input("Digite o segundo numero: "))
+divisao_inteira = primeiro_numero // segundo_numero
+print(f"A divisão inteira de {primeiro_numero} dividido por {segundo_numero} é : {divisao_inteira}")
+
+# 5 - Escreva um programa que calcule o quadrado de um número fornecido pelo usuário.
+numero = int(input("Digite o numero: "))
+quadrado = numero ** 2
+print(f"O quadrado de {numero} é : {quadrado}")
+
+NÚMEROS DE PONTO FLUTUANTE (float)
+
+https://github.com/lvgalvao/data-engineering-roadmap/tree/main/Bootcamp%20-%20Python%20para%20dados/aula02
+
+'''
+
+# 5 - Escreva um programa que calcule o quadrado de um número fornecido pelo usuário.
+numero = int(input("Digite o numero: "))
+quadrado = numero ** 2
+print(f"O quadrado de {numero} é : {quadrado}")
