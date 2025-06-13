6e15e1dddd411b63a797c0db27c4725185b74000
Willian Garcia <webwil2012@gmail.com>
2025-06-12 23:18:37 -0300

    Iniciando trabalho com eeros

---
+++ 02_tabalhando_com_erros/erros.py
@@ -0,0 +1,9 @@
+valor = int(0)
+divisao = int(0)
+
+try:
+    resultado = valor / divisao
+    print(resultado)
+except:
+    resultado = "Erro: integer division or modulo by zero Willian"
+    print(resultado)
