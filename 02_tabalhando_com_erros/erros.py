6b1b7a69dc5c15ee0cffa9336734994643611ff4
Willian Garcia <webwil2012@gmail.com>
2025-06-15 17:20:05 -0300

    Atualização de arquivo erros.py e criação de arquivo erros_2.py

--- 02_tabalhando_com_erros/erros.py
+++ 02_tabalhando_com_erros/erros.py
@@ -1,9 +1,21 @@
-valor = int(0)
-divisao = int(0)
+valor = int(5)
+divisao = int(1)
 
 try:
     resultado = valor / divisao
     print(resultado)
-except:
+except ZeroDivisionError:
     resultado = "Erro: integer division or modulo by zero Willian"
     print(resultado)
+except KeyboardInterrupt:
+    resultado = "Acho que você interrompeu o programa"
+    print(resultado)
+    
+try:
+    resultado = len("5")
+except TypeError as e:
+    print(e)
+else:
+    print("eu tudo certo com o programa")
+finally:
+    print("Finalizando o programa")
---
+++ 02_tabalhando_com_erros/erros_2.py
@@ -0,0 +1,5 @@
+numero = int(input("Digite um numero: "))
+if isinstance(numero, int):
+    print("A variável é um inteiro.")
+else:
+    print("A variável não é um inteiro.")
